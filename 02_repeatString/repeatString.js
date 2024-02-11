87390609862411fea64033918bef3f57f4019ff2
lentils <vancekuikahi@gmail.com>
2024-02-11 14:53:29 -0800

    soln2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTimes) {
+    if (repeatTimes >= 0) {
+        return string.repeat(repeatTimes)
+    } else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
