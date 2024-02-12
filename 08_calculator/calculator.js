6bf5890f29f82e1f1f39faded9514f422ba72b86
lentils <vancekuikahi@gmail.com>
2024-02-12 00:53:28 -0800

    soln 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const total = arr.reduce((total, num) => {
+    return total += num;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const total = arr.reduce((total, num) => {
+    return total *= num;
+  }, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let total = 1;
+  for(let i = 0; i < exponent; i++) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    let total = 1;
+    for (let i = num; i > 0; i--) {
+      total = total * i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
