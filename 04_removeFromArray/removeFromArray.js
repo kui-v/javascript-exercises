3d0c70edb87528925c42b1d7740b42afc0a30da5
lentils <vancekuikahi@gmail.com>
2024-02-11 19:06:05 -0800

    soln 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        arr = arr.filter(
+            (x) => x !== arguments[i]
+        );
+        console.log(arr);
+        // let index = arr.indexOf(arguments[i]);
+        // arr.splice(index, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
