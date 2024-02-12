51b717281a1de10626a75470cf554d58359f8f9c
lentils <vancekuikahi@gmail.com>
2024-02-11 19:33:56 -0800

    temp conversion soln

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let conversion = ((degrees - 32) * (5/9))
+  if (conversion % 1 === 0) {
+    return conversion
+  } else {
+    return Number(conversion.toFixed(1))
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let conversion = ((degrees * (9/5)) + 32)
+  if (conversion % 1 === 0) {
+    return conversion
+  } else {
+    return Number(conversion.toFixed(1))
+  }
 };
 
 // Do not edit below this line
